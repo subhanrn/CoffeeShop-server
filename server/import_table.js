4fd26169b3496c6ce24558562b1819ba9e346d64
subhanrn <subhanrn@outlook.com>
2019-03-23 15:18:22 +0700

    cleaner code

--- server/import_table.js
+++ server/import_table.js
@@ -1,28 +1,29 @@
-var loopback = require('loopback');
-var ds = loopback.createDataSource('oracle', {
-  "host": "oracle-demo.strongloop.com",
-  "port": 1521,
-  "database": "XE",
-  "username": "demo",
-  "password": "L00pBack"
+'use strict';
+const loopback = require('loopback');
+const ds = loopback.createDataSource('oracle', {
+  'host': 'oracle-demo.strongloop.com',
+  'port': 1521,
+  'database': 'XE',
+  'username': 'demo',
+  'password': 'L00pBack',
 });
 
 // Discover and build models from INVENTORY table
-ds.discoverAndBuildModels('INVENTORY', { visited: {}, associations: true },
-  function (err, models) {
+ds.discoverAndBuildModels('INVENTORY', {visited: {}, associations: true},
+  (err, models) => {
     // Now we have a list of models keyed by the model name
     // Find the first record from the inventory
-    models.Inventory.findOne({}, function (err, inv) {
+    models.Inventory.findOne({}, (err, inv) => {
       if (err) {
         console.error(err);
         return;
       }
-      console.log("\nInventory: ", inv);
+      console.log('\nInventory: ', inv);
       // Navigate to the product model
       // Assumes inventory table has a foreign key relationship to product table
-      inv.product(function (err, prod) {
-        console.log("\nProduct: ", prod);
-        console.log("\n ------------- ");
+      inv.product((err, prod) => {
+        console.log('\nProduct: ', prod);
+        console.log('\n ------------- ');
       });
     });
   });
