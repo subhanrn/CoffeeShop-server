5c84151f49ca384b0656036b08da9bf2de62b7f4
Nahal <subhanrn@outlook.com>
2019-04-13 16:26:14 +0700

    complete update

--- common/models/coffee-service.js
+++ common/models/coffee-service.js
@@ -186,4 +186,85 @@ module.exports = (CoffeeService) => {
       });
     });
   };
+
+  CoffeeServices.remoteMethod('upsertWithWhereReview', {
+    accepts: [{
+      arg: 'params',
+      type: 'object',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/upsertWithWhereReview',
+      verb: 'post',
+    },
+  });
+
+  CoffeeServices.upsertWithWhereReview = (params) => {
+    const reviewModel = CoffeeServices.app.models.Review;
+    return new Promise((res, rej) => {
+      reviewModel.upsertWithWhere({
+        coffeeShopId: params.coffeeShopId,
+        publisherId: params.publisherId,
+      }, {
+        date: Date.now(),
+        rating: params.rating,
+        comments: params.comments,
+        coffeeShopId: params.coffeeShopId,
+        publisherId: params.publisherId,
+      }, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  CoffeeServices.remoteMethod('getMyReview', {
+    accepts: [{
+      arg: 'cont',
+      type: 'object',
+      required: true,
+      http: ctx => ctx && ctx.req,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'array',
+      root: true,
+    },
+    http: {
+      path: '/getMyReview',
+      verb: 'get',
+    },
+  });
+
+  CoffeeServices.getMyReview = (cont) => {
+    const {
+      accessToken,
+    } = cont;
+    const {
+      userId,
+    } = accessToken;
+    const reviewModel = CoffeeServices.app.models.Review;
+    return new Promise((resolve, reject) => {
+      reviewModel.find({
+        where: {
+          publisherId: userId,
+        },
+      }, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
 };
