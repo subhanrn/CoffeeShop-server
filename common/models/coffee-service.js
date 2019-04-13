70b999ace04b18c96ddf1e2649d318ad908f85d3
Nahal <subhanrn@outlook.com>
2019-04-13 15:28:12 +0700

    add new remote methods

--- common/models/coffee-service.js
+++ common/models/coffee-service.js
@@ -1,1 +1,191 @@
-module.exports = () => {};
+const app = require('../../server/server');
+
+module.exports = (CoffeeService) => {
+  const CoffeeServices = CoffeeService;
+
+
+  CoffeeServices.remoteMethod('findAllCoffeeShop', {
+    accepts: [],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/findAllCoffeeShop',
+      verb: 'get',
+    },
+  });
+
+  CoffeeServices.findAllCoffeeShop = () => {
+    const coffeeShopModel = app.models.CoffeeShop;
+    return new Promise((res, rej) => {
+      coffeeShopModel.find(null, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  CoffeeServices.remoteMethod('findByIdCoffeeShop', {
+    accepts: [{
+      arg: 'id',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/findByIdCoffeeShop',
+      verb: 'get',
+    },
+  });
+
+  CoffeeServices.findByIdCoffeeShop = (id) => {
+    const coffeeShopModel = app.models.CoffeeShop;
+    return new Promise((res, rej) => {
+      coffeeShopModel.find({
+        where: {
+          id,
+        },
+      }, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  CoffeeServices.remoteMethod('createCoffeeShop', {
+    accepts: [{
+      arg: 'params',
+      type: 'object',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/createCoffeeShop',
+      verb: 'post',
+    },
+  });
+
+  CoffeeServices.createCoffeeShop = (params) => {
+    const coffeeShopModel = app.models.CoffeeShop;
+    return new Promise((res, rej) => {
+      coffeeShopModel.create(params, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+  CoffeeServices.remoteMethod('updateByIdCoffeeShop', {
+    accepts: [{
+      arg: 'params',
+      type: 'object',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/updateByIdCoffeeShop',
+      verb: 'post',
+    },
+  });
+
+  CoffeeServices.updateByIdCoffeeShop = (params) => {
+    const coffeeShopModel = app.models.CoffeeShop;
+    return new Promise((res, rej) => {
+      coffeeShopModel.replaceById(params.id, {
+        name: params.name,
+        city: params.city,
+      }, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  CoffeeServices.remoteMethod('deleteByIdCoffeeShop', {
+    accepts: [{
+      arg: 'id',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/deleteByIdCoffeeShop',
+      verb: 'get',
+    },
+  });
+
+  CoffeeServices.deleteByIdCoffeeShop = (id) => {
+    const coffeeShopModel = app.models.CoffeeShop;
+    return new Promise((res, rej) => {
+      coffeeShopModel.destroyById(id, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  CoffeeServices.remoteMethod('findAllReview', {
+    accepts: [],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/findAllReview',
+      verb: 'get',
+    },
+  });
+
+  CoffeeServices.findAllReview = () => {
+    const reviewModel = app.models.Review;
+    return new Promise((res, rej) => {
+      reviewModel.find({
+        include: ['coffeeShop', 'reviewer'],
+      }, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+};
--- common/models/user-service.js
+++ common/models/user-service.js
@@ -2,19 +2,13 @@ const app = require('../../server/server');
 
 module.exports = (UserService) => {
   const Userservices = UserService;
+
   Userservices.remoteMethod('createUser', {
     accepts: [{
-        arg: 'params',
-        type: 'object',
-        required: true,
-      },
-      {
-        arg: 'cont',
-        type: 'object',
-        required: true,
-        http: ctx => ctx && ctx.req,
-      },
-    ],
+      arg: 'params',
+      type: 'object',
+      required: true,
+    }],
     returns: {
       arg: 'return',
       type: 'object',
@@ -26,10 +20,10 @@ module.exports = (UserService) => {
     },
   });
 
-  Userservices.createUser = async (params) => {
-    const reviewer = app.models.Reviewer;
-    const newuser = await new Promise((res, rej) => {
-      reviewer.create(params, (err, result) => {
+  Userservices.createUser = (params) => {
+    const reviewerModel = app.models.Reviewer;
+    return new Promise((res, rej) => {
+      reviewerModel.create(params, (err, result) => {
         if (err) {
           rej(err);
         } else {
@@ -37,5 +31,75 @@ module.exports = (UserService) => {
         }
       });
     });
+  };
+
+
+  Userservices.remoteMethod('signIn', {
+    accepts: [{
+      arg: 'params',
+      type: 'object',
+      required: true,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/signIn',
+      verb: 'post',
+    },
+  });
+
+  Userservices.signIn = (params) => {
+    const reviewerModel = app.models.Reviewer;
+    return new Promise((res, rej) => {
+      reviewerModel.login(params, (err, result) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(result);
+        }
+      });
+    });
+  };
+
+
+  Userservices.remoteMethod('signOut', {
+    accepts: [{
+      arg: 'cont',
+      type: 'object',
+      required: true,
+      http: ctx => ctx && ctx.req,
+    }],
+    returns: {
+      arg: 'return',
+      type: 'object',
+      root: true,
+    },
+    http: {
+      path: '/signOut',
+      verb: 'post',
+    },
+  });
+
+  Userservices.signOut = (cont) => {
+    const accessTokenModel = app.models.AccessToken;
+    const {
+      accessToken,
+    } = cont;
+    if (accessToken == null) {
+      throw new Error('invalid token');
+    } else {
+      return new Promise((res, rej) => {
+        accessTokenModel.destroyById(accessToken.id, (err, result) => {
+          if (err) {
+            rej(err);
+          } else {
+            res(result);
+          }
+        });
+      });
+    }
   };
 };
